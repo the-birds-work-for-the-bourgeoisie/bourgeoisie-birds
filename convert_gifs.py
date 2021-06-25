64043e555327f5ab0ae1e5e029c2342e12bf6364
Hunter Wilhelm <zicy@zicy.net>
2021-06-25 09:17:58 -0600

    answer sprite

--- convert_gifs.py
+++ convert_gifs.py
@@ -26,7 +26,7 @@ def convert_frame_to_transparent(imageObject: GifImageFile) -> Image:
     data = np.array(rgbaImage)  # "data" is a height x width x 4 numpy array
     red, green, blue, alpha = data.T  # Temporarily unpack the bands for readability
 
-    # Replace white with red... (leaves alpha values alone...)
+    # Replace white with clear... (leaves alpha values alone...)
     white_areas = (red == 255) & (blue == 255) & (green == 255)
     data[...][white_areas.T] = (0, 0, 0, 0)  # Transpose back needed
 
